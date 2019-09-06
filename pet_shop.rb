b70dea38fca178034e8867918e397a96c83d14db
Michael Crockett <mike.crockett76@gmail.com>
2019-09-06 12:57:51 +0100

    Second commit

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,5 +1,11 @@
 def pet_shop_name(name)
   for name in @pet_shop
     return @pet_shop[:name]
+  end
 end
+
+def total_cash(cash_total)
+  for cash_total in @pet_shop
+    return @pet_shop[:admin][:total_cash]
+  end
 end
