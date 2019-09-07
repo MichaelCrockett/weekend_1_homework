96bbcd61783f9215a934978335f302a5edb966ac
Michael Crockett <mike.crockett76@gmail.com>
2019-09-07 12:48:28 +0100

    First saturday commit

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,6 @@
+require ('pry')
+
+
 def pet_shop_name(name)
   for name in @pet_shop
     return @pet_shop[:name]
@@ -12,4 +15,22 @@ end
 
 def add_or_remove_cash(add, amount)
 @pet_shop[:admin][:total_cash] += amount
+end
+#
+def pets_sold(pets_sold)
+  for pets_sold in @pet_shop
+    return @pet_shop[:admin][:pets_sold]
+  end
+end
+
+def increase_pets_sold(add_sold, number)
+  for add_sold in @pet_shop
+    return @pet_shop[:admin][:pets_sold] += number
+  end
+end
+
+def stock_count(count)
+  for count in @pet_shop
+    return @pet_shop[:pets].count
+  end
 end
