c10e02969bff82e55e527b5d57f068205afcdbe0
Michael Crockett <mike.crockett76@gmail.com>
2019-09-08 14:50:56 +0100

    final commit

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,6 +1,3 @@
-require ('pry')
-
-
 def pet_shop_name(name)
   for name in @pet_shop
     return @pet_shop[:name]
@@ -14,7 +11,8 @@ def total_cash(cash_total)
 end
 
 def add_or_remove_cash(add, amount)
-@pet_shop[:admin][:total_cash] += amount
+  @pet_shop[:admin][:total_cash] += amount
+    return @pet_shop[:admin][:total_cash]
 end
 #
 def pets_sold(pets_sold)
@@ -30,7 +28,50 @@ def increase_pets_sold(add_sold, number)
 end
 
 def stock_count(count)
-  for count in @pet_shop
-    return @pet_shop[:pets].count
-  end
+  return @pet_shop[:pets].count
 end
+
+
+# def all_pets_by_breed
+# end
+
+def find_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      return pet
+    end
+  end
+  return nil
+end
+
+#
+# def find_pet_by_name
+# end
+#
+# def remove_pet_by_name
+# end
+
+def add_pet_to_stock (shop, new_pet)
+ @pet_shop[:pets] << @new_pet
+    def stock_count(shop)
+   return @pet_shop [:pets].count
+ end
+end
+
+def customer_cash (customer)
+ return customer[:cash]
+end
+#
+def remove_customer_cash (customer, amount)
+ customer[:cash]= customer[:cash]-= amount
+end
+
+
+# def remove_customer_cash
+# end
+
+# def customer_pet_count
+# end
+
+# def add_pet_to_customer
+# end
